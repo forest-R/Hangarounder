9509791cda98a4803d876f07c17869a8634da7c8
forest-R <121208411+forest-R@users.noreply.github.com>
2026-04-29 10:12:51 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        forest: {
+          50:  "#f8faf5",
+          100: "#EAF3DE",
+          200: "#C0DD97",
+          400: "#97C459",
+          600: "#639922",
+          800: "#3B6D11",
+          900: "#27500A",
+        }
+      }
+    }
+  },
+  plugins: []
+};
